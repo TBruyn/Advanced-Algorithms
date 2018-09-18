967f214d95e3674f288f4240e220f25883c7a9d9
TBruyn <timtbruyn@gmail.com>
2018-09-18 13:40:45 +0200

    Add getSubset and update methods

--- src/TardinessCalculator.java
+++ src/TardinessCalculator.java
@@ -1,5 +1,6 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 
 public class TardinessCalculator {
@@ -8,6 +9,13 @@ public class TardinessCalculator {
     First row is processing time, second row is due dates
      */
     private int[][] jobs;
+    private int[][] testjobs = new int[][]{
+            {98, 314},
+            {26, 287},
+            {82, 285},
+            {67, 253},
+            {85, 256}
+    };
 
     public static void main(String args[]) {
         if (args.length == 0) {
@@ -27,35 +35,44 @@ public class TardinessCalculator {
 
     private int calculateTardiness(int i, int j, int k, int t) {
 
+        if (i == j) return jobs[0][i];
+
         int minimalTardiness = Integer.MAX_VALUE;
         for (int delta = k; delta < jobs.length; delta++) {
 
-            int kprime = findBiggestJob(getSubset(i, j, k));
-            int cprime = calculateCompletionTime(
-                    t,
-                    getSubset(i, k + delta, kprime));
+            int kPrime = findBiggestJob(getSubset(i, j, k));
+            int cPrime = calculateCompletionTime(
+                    t, getSubset(i, k + delta, kPrime));
 
-            int subsetTardiness = calculateTardiness(i, k + delta, kprime, t)
-                    + Integer.max(0, cprime - jobs[1][kprime])
-                    + calculateTardiness(kprime + delta + 1, j, kprime, cprime);
+            int subsetTardiness = calculateTardiness(i, k + delta, kPrime, t)
+                    + Integer.max(0, cPrime - jobs[1][kPrime])
+                    + calculateTardiness(kPrime + delta + 1, j, kPrime, cPrime);
             if (minimalTardiness > subsetTardiness)
                 minimalTardiness = subsetTardiness;
         }
         return minimalTardiness;
     }
 
-    private int[] calculateDeltas() {
-        return new int[]{};
+    private ArrayList<Integer> calculateDeltas(ArrayList<Integer> set) {
+        int k = findBiggestJob(set);
+        return new ArrayList<Integer>();
     }
 
     /*
     S
      */
-    private int[] getSubset(int i, int j, int k) {
-        return new int[]{1};
+    private ArrayList<Integer> getSubset(int i, int j, int k) {
+        ArrayList<Integer> subset = new ArrayList<>();
+
+        for (int id = i; id <= j; id++) {
+            if (jobs[0][id] < jobs[0][k]) {
+                subset.add(id);
+            }
+        }
+        return subset;
     }
 
-    private int calculateCompletionTime(int startingTime, int[] set) {
+    private int calculateCompletionTime(int startingTime, ArrayList<Integer> set) {
 
         int productionTimeSum = 0;
         for (int id : set)
@@ -64,7 +81,7 @@ public class TardinessCalculator {
         return startingTime + productionTimeSum;
     }
 
-    private int findBiggestJob(int[] set) {
+    private int findBiggestJob(ArrayList<Integer> set) {
         int max = 0;
         int idOfMax = 0;
 
